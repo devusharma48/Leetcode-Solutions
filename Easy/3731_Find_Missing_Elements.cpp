d70c78eae238284f581eabed9594e4136d704388
Divyansh Sharma <divyansh777sharma@gmail.com>
2026-08-04 23:42:50 +0530

    Add findMissingElements method to Solution class
    This implementation identifies and returns all missing elements in a given range based on the input vector.

---
+++ Easy/3731_Find_Missing_Elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int mn = INT_MAX;
+        int mx = INT_MIN;
+
+        unordered_set<int> s;
+
+        for (int x : nums) {
+            mn = min(mn, x);
+            mx = max(mx, x);
+            s.insert(x);
+        }
+
+        vector<int> ans;
+
+        for (int i = mn + 1; i < mx; i++) {
+            if (!s.count(i))
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
