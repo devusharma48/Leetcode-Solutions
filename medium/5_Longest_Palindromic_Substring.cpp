43ec903c40594ec14e9c6243122a5ef025a78248
Divyansh Sharma <divyansh777sharma@gmail.com>
2026-08-15 23:49:11 +0530

    Add solution for longest palindromic substring

---
+++ medium/5_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0, maxLen = 1;
+
+        auto expand = [&](int l, int r) {
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+                l--;
+                r++;
+            }
+
+            int len = r - l - 1;
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = l + 1;
+            }
+        };
+
+        for (int i = 0; i < s.size(); i++) {
+            expand(i, i);       
+            expand(i, i + 1);   
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
