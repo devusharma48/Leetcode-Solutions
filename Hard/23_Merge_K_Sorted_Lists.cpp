de40458c45a1023ff4bc640b5c88822c167046bf
Divyansh Sharma <divyansh777sharma@gmail.com>
2026-09-02 23:50:19 +0530

    Add solution for LeetCode 23 - Merge k Sorted Lists.

---
+++ Hard/23_Merge_K_Sorted_Lists.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> minHeap;
+
+        for (ListNode* head : lists) {
+            if (head != nullptr) {
+                minHeap.push(head);
+            }
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (!minHeap.empty()) {
+            ListNode* smallest = minHeap.top();
+            minHeap.pop();
+
+            tail->next = smallest;
+            tail = tail->next;
+
+            if (smallest->next != nullptr) {
+                minHeap.push(smallest->next);
+            }
+        }
+
+        return dummy.next;
+    }
+};
